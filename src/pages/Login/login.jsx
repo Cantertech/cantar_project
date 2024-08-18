d578a847b259497d3712b7911de05c1de51927ea
HashTag-star <naseefhamza1@gmail.com>
2024-08-18 22:20:12 +0000

    first commit

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 
 const Login = () => {
+
+  const [CurrState, setCurrState ] = useState ("Sign Up")
+
   return (
     <div className='login'>
-        <img src="" alt="" />
-        div
+      <div className="login-logo">
+      <img src="src/assets/ecommerce-svgrepo-com.png" alt="" />
+      <p>CantarTech</p>
+      </div>
+      <form className='login-form'>
+        <h2>{CurrState}</h2>
+        {CurrState === "Sign Up"?<input type="text" placeholder='Username' className='form-input' required/>: null}
+        <input type="email" placeholder='Email' className='form-input' required/>
+        <input type="password" placeholder='Password' className='form-input' required/>
+        <button>{CurrState === "Sign Up"?"Create account":"Login now" }</button>
+        {CurrState === "Sign Up"?
+
+        <div className="long-term">
+          <input type="checkbox"  required/>
+          <p>Agree to the term of use & privacy policy. </p>
+        </div>
+        :
+         <p className='forgot-password'>Forgot password</p>
+        }
+        <div className='login-forgot'>
+          { CurrState === "Sign Up"?
+
+            <p className='login-toggle'>Already have an account? <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+            :
+            <p className='login-toggle'>Don't have an account? <span onClick={()=>setCurrState("Sign Up")}>Sign up</span></p>
+          }
+        </div>
+      </form>
     </div>
   )
 }
--- src/pages/Products/products.jsx
+++ src/pages/Products/products.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import './products.css'
 
 const products = () => {
+
   return (
     <div className='products'>
         <div className="nav">
             <div className="items">
-                <img src="" className='logo' alt="soon" />
-                <input type="search" placeholder='search' />
-                <img src="src\assets\search_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" className='srch-btn' alt="" />
+                <div className='logo-element'>
+                    <img src="/logo-main.svg" className='logo' alt="soon" />
+                    <p className='canta'>CantarTech</p>
+                </div>
+                <div className='srch-element'>
+                    <input type="search" placeholder='search' />
+                    <img src="src\assets\search_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" className='srch-btn' alt="" />
+                </div>
                 <ul>
                     <li><a href=""><img src="src\assets\home_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" alt="" /></a></li>
                     <li><a href=""><img src="src\assets\shopping_bag_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png" alt="" /></a></li>
@@ -19,7 +25,9 @@ const products = () => {
         </div>
         <hr/>
         <div className="product-display">
-            <div className="banner"></div>
+            <div className="banner">
+                
+            </div>
             <div className="first-line"></div>
         </div>
     </div>
